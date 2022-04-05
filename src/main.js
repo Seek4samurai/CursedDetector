89aaaf499a8c5a23ae800f153bae9fca4e0dd5f8
Gourav <grvrawat999@gmail.com>
2022-04-05 14:48:28 +0530

    fix: post request

--- src/main.js
+++ src/main.js
@@ -1,52 +1,6 @@
-const audio = document.getElementById("audio");
-const mainAudio = document.createElement("audio");
-const paragraph = document.getElementById("para");
-const form = document.querySelector("form");
-const startBtn = document.getElementById("startBtn");
 const submitBtn = document.getElementById("submitBtn");
+const form = document.querySelector("form");
 
-// Preventing from refresh
-startBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-});
-// -----------------------------------------
-
-var items = [];
-
-const start = async () => {
-  var device = navigator.mediaDevices.getUserMedia({ audio: true });
-  device.then((stream) => {
-    var recorder = new MediaRecorder(stream);
-
-    recorder.ondataavailable = async (e) => {
-      items.push(e.data);
-      if (recorder.state == "inactive") {
-        var blob = new Blob(items, { type: "audio/webm" });
-
-        mainAudio.setAttribute("controls", "controls");
-        audio.appendChild(mainAudio);
-
-        mainAudio.innerHTML =
-          '<source src="' + URL.createObjectURL(blob) + '" type="audio/webm"/>';
-
-        const audioURL = URL.createObjectURL(blob);
-        console.log(audioURL);
-
-        const formData = new FormData();
-        formData.append("files", "hey hey");
-
-        const res = await axios.post(
-          "http://127.0.0.1:5000/response",
-          formData
-        );
-        console.log(res.data);
-      }
-    };
-    recorder.start(100);
-    console.log("started!");
-
-    setTimeout(() => {
-      recorder.stop();
-    }, 5000);
-  });
-};
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+// });
